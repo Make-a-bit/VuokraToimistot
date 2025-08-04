ae2c157d1f1e31379bf22f73e1cd583579829aea
Janne Toivonen <janne.toivonen@edu.karelia.fi>
2025-08-04 07:45:21 +0300

    Bug hunt

--- client/src/pages/customers.js
+++ client/src/pages/customers.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Alert, Button, Snackbar } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { addCustomer, editCustomer } from "../redux/actions/customerActions";
