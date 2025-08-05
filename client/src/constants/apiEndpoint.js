d15f971b9e8d3595c223b8a86a1ef2481a96fc26
Janne Toivonen <janne.toivonen@edu.karelia.fi>
2025-08-05 11:26:19 +0300

    Updated apiEndpoint

--- client/src/constants/apiEndpoint.js
+++ client/src/constants/apiEndpoint.js
@@ -2,6 +2,6 @@
  * 
  * @type {string}
  */
-const mainURI = "https://vuokratoimistot-fnd5b9dna3c6apcc.swedencentral-01.azurewebsites.net"
+const mainURI = "https://vtapi-a3dfhadydmcpg7hf.northeurope-01.azurewebsites.net"
 
 export default mainURI;
